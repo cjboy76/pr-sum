0a0843de975ba939caeb5ddf1d6266e76c59d332
CJBoy <78361904+cjboy76@users.noreply.github.com>
2025-01-15 17:53:27 +0800

    wip: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import path from "path"
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
   return {
-    define: {
-      'process.env.GITHUB_TOKEN': env.VITE_GITHUB_TOKEN
-    },
     plugins: [react()],
     resolve: {
       alias: {
